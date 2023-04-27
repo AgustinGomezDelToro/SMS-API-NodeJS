6ebd710510bfef66d06cd25d5eea2ab2997d2754
AgustinGomezDelToro <agustinngomezdeltoro@gmail.com>
2023-04-27 11:27:25 +0200

    API SMSPARTNER AVEC NodeJS

--- api.js
+++ api.js
@@ -1,26 +1,45 @@
-var http = require("http");
-var options = {
+// Importer le module 'https' de Node.js
+const https = require('https');
+
+// Objet JSON qui contient les informations nécessaires pour envoyer le SMS
+const data = JSON.stringify({
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', //remplacez par votre clé API
+    phoneNumbers: '+336XXXXXXXX',  //remplacez par votre numéro de téléphone
+    sender: 'NodeJS',
+    gamme: 1,
+    message: "Cest un message test NodeJS" //remplacez par votre message
+});
+
+// Définir les options pour la requête HTTP POST vers l'API SMS Partner
+const options = {
   hostname: 'api.smspartner.fr',
   port: 443,
   path: '/v1/send',
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-
-    'cache-control': 'no-cache'
+    'Content-Length': data.length,
+    'cache-control': 'no-cache',
+    'webhookUrl': 'https://webhook.site/TOKEN' // Webhook URL *cette ligne est optionnel*
   }
 };
-var req = http.request(options, function (res) {
-var chunks = [];
-res.on("data", function (chunk) {
-chunks.push(chunk);
+
+// Demande HTTP POST avec les options et les données définies précédemment
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+  
+  // Afficher les données de réponse de l'API sur la sortie standard
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
 });
-res.on("end", function () {
-var body = Buffer.concat(chunks);
-console.log(body.toString());
+
+// Affichage en cas d'erreur lors de l'exécution de la requête HTTP POST
+req.on('error', (error) => {
+  console.error(error);
 });
-});
-req.write(JSON.stringify({ apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-phoneNumbers: '+336XXXXXXX', sender: 'demo',
-gamme: 1, message: 'C\'est un message test' }));
+
+// Envoyer les données de l'objet 'data' à la demande
+req.write(data);
+// Terminer la demande HTTP POST
 req.end();
