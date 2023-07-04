5f870e40e2bab17faa38a6812e081abe77e24e4e
AgustinGomezDelToro <agustinngomezdeltoro@gmail.com>
2023-07-04 15:49:10 +0200

    Récupérer la liste des sous-comptes en NodeJS

--- Récupération-des-sousComptes.js
+++ Récupération-des-sousComptes.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-// Préparer les données pour la requête GET
+// Permets de récupérer la liste des sous-comptes
 let data = 'apiKey=YOUR_API_KEY';
 let url = 'https://api.smspartner.fr/v1/subaccount/list?' + data;
 
